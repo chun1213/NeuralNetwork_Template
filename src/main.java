4db31f2347c421dcce137936de42f3b1d6dd82cc
chun1213 <53665251+chun1213@users.noreply.github.com>
2020-07-31 13:02:31 -0400

    update

--- src/main.java
+++ src/main.java
@@ -15,16 +15,26 @@ import org.apache.commons.math3.linear.*;
 
 
 
+
+
+
+
+//NN Template
+//Created by Chunfeng Li
+//Released 07/30/2020
+//This is simply a template for a Feed Forward network, NO TRAINING is happening
+
+
+
+
+
 public class main {
-	int evonum=50;
-	int popsize=30;
+	
 	static int outputLayersNode=10;
 	static int inputLayersNode=784;
 	static int numHiddenLayers=2;
 	static int HiddenLayersNode=200;
-	static int trainingSize=10000;
-	static double learningRate=0.00001;
-	static double right=0;
+	
 	static File storage=new File("brain.txt");
 	//input layer nodes ,number of hidden layers, nodes in hidden layers,output layer nodes
 	
@@ -33,238 +43,23 @@ public class main {
 		 NeuralNetwork n  = new NeuralNetwork(inputLayersNode,numHiddenLayers,HiddenLayersNode,outputLayersNode);
 		 System.out.println("success");
 		
-		 //Training data
-//		 MnistMatrix[] mnistMatrix = new MnistDataReader().readData("data/train-images.idx3-ubyte", "data/train-labels.idx1-ubyte");
-		 
-		 //Test data
-		 MnistMatrix[] mnistMatrix = new MnistDataReader().readData("data/t10k-images.idx3-ubyte", "data/t10k-labels.idx1-ubyte");
+		
 		 
 		 
 		 //simply comment this out to start a new NN!
 		 n=readFromFile(storage);
 		 
-		 
-		 
-		 for (int x=0;x<trainingSize;x++) {
-			 System.out.println(x);
-			 //Gets training data ThreadLocalRandom.current().nextInt(0, 59999 + 1)
-			 MnistMatrix matrix=mnistMatrix[x];
-			  int label=matrix.getLabel();
-			  //gets the answer Array
-			  double[] answer=labelToArray(label);
-			  
-			  double[] input = new double[784];
-			  int counter=0;
-		        for (int r = 0; r < matrix.getNumberOfRows(); r++ ) {
-		            for (int c = 0; c < matrix.getNumberOfColumns(); c++) {
-		                input[counter]=matrix.getValue(r, c);
-		                counter++;
-		            }
-		            
-		        }
-		        
-		        //
-			 
-			 double[] output=n.Evaluate(input);
-			 ArrayList<Layer> networkLayers= n.getLayer();
-			 
-			 
-			 
-			 
-			 //finding cost variable
-			 double tempcost;
-			 double cost[]= new double[output.length];
-			 for (int i=0;i<output.length;i++) {
-				 tempcost=answer[i]-output[i];
-				 cost[i]=tempcost;
-			 }
-	
-//			 System.out.print(Arrays.toString(output));
-//			 double avgCost=0;
-//			 for (int i=0;i<cost.length;i++) {
-//				 avgCost+=cost[i];
-//			 }
-//			 avgCost=avgCost/cost.length;
-			 //System.out.println(avgCost);
-			
-			 if (label==getAnswer(output)) {
-				 System.out.println(true);
-				 right++;
-			 }
-			 else {
-				 System.out.println(getAnswer(output));
-				 System.out.println(label);
-			 }
-			 
-			 
-			 
-			 RealMatrix Cost = new Array2DRowRealMatrix(cost);
-			 
-			 
-			//derivative sigmoid output 
-			 double dOutput[][]= dSigmoid(output);
-//			 RealMatrix dOutputM = new Array2DRowRealMatrix(dOutput);
-			 
-			 //getting gradient
-			 
-			 
-			 
-//			 System.out.println(Arrays.toString(output));
-			 
-			 
-			 
-			 
-			 
-			 
-			 
-			 
-			 
-			 double[][] gradient=Multiply(Cost.getData(),dOutput);
-			 gradient=Multiply(gradient,learningRate);
-			
-			 
-			
-			 RealMatrix gradientM = new Array2DRowRealMatrix(gradient);
-			 
-			 
-			 
-			 ArrayList<Node> prevNodes =networkLayers.get(networkLayers.size()-2).getNodes();
-			
-			 
-			 double[] values= new double[prevNodes.size()];
-			 for (int j=0;j<prevNodes.size();j++) {
-				 values[j]=prevNodes.get(j).getValue();
-			 }
-			 RealMatrix prevLayerOut = new Array2DRowRealMatrix(values);
-			 prevLayerOut=prevLayerOut.transpose();
-			 
-			 
-			 
-			 
-			 
-			 
-			 
-			 
-			 //Obtaining delta
-			 gradientM=gradientM.multiply(prevLayerOut);
-			 
+		 //INPUT for network as an ARRAY
+		double[] input = new double[784];
+		
+		//Output of network as an ARRAY
+		double[] output=n.Evaluate(input);
 			 
 
-			 
-			 
-			 
-			 
-			 ArrayList<Node> currentNodes =networkLayers.get(networkLayers.size()-1).getNodes();
-			 
-			 //Changes the values
-			 for (int j=0;j<prevNodes.size();j++) {
-				 double[] deltas=gradientM.getColumn(j);
-				 prevNodes.get(j).changeOutWeights(deltas);
-				 
-
-			 }
-			
-			 for (int j=0;j<currentNodes.size();j++) {
-				 
-				currentNodes.get(j).changeBias(gradient[j][0]);
-				 
-				
-			 }
-			 
-			 
-			
-			 RealMatrix costPreviousLayer=Cost;
-			 
-			 
-			 
-			 
-			 
-			 
-			 
-			 
-			 //Loops for each layer of the neural network except the last
-			 for (int i=networkLayers.size()-2;i>0;i-- ) {
-				 
-				 //Obtains the nodes in that layer and previous
-				 ArrayList<Node> nodes =networkLayers.get(i).getNodes();
-				 ArrayList<Node> PrevNodes =networkLayers.get(i-1).getNodes();
-				 int forwardNodeNum =networkLayers.get(i+1).getNodes().size();
-				
-				 RealMatrix weights=new Array2DRowRealMatrix(forwardNodeNum,nodes.size());
-				 double[] currentHiddenOutputs=new double[nodes.size()];
-				 //Loops through each node
-				 for (int t=0;t<nodes.size();t++ ) {
-					 Node currentNode=nodes.get(t);
-					 
-					 //puts weights and values of the node into array column
-					 double tempWeight[]= currentNode.getOutWeights();
-					 weights.setColumn(t,tempWeight);
-					 double tempValue= currentNode.getValue();
-					 currentHiddenOutputs[t]=tempValue;
-				 }
-				 //gets outputs of prevNodes
-				 double prevHiddenOutputs[]=new double[PrevNodes.size()];
-				 for (int t=0;t<PrevNodes.size();t++ ) {
-					
-					 Node currentNode=PrevNodes.get(t);
-					 
-					 //puts Value of the node into array column
-					 double tempValue= currentNode.getValue();
-					 prevHiddenOutputs[t]=tempValue;
-				 }
-				 
-				 RealMatrix weights_t=weights.transpose();
-				 
-				 //hidden layer error
-				 RealMatrix hiddenErrors = weights_t.multiply(costPreviousLayer);
-				
-				 
-				 //gradient
-				 
-				 double[][] hiddenGradient=dSigmoid(currentHiddenOutputs);
-				 hiddenGradient=Multiply(hiddenGradient,hiddenErrors.getData());
-				 RealMatrix hiddenGradientM=new Array2DRowRealMatrix(Multiply(hiddenGradient, learningRate));
-				 
-				 RealMatrix PrevLayerOut = new Array2DRowRealMatrix(prevHiddenOutputs);
-				 PrevLayerOut=PrevLayerOut.transpose();
-				 
-				 //Obtaining delta
-				 hiddenGradientM=hiddenGradientM.multiply(PrevLayerOut);
-				
-				 
-				currentNodes =networkLayers.get(networkLayers.size()-1).getNodes();
-				 //changeing values
-				 for (int j=0;j<PrevNodes.size();j++) {
-					 double[] deltas=hiddenGradientM.getColumn(j);
-					 PrevNodes.get(j).changeOutWeights(deltas);
-					
-				 }
-				 
-				 for (int j=0;j<currentNodes.size();j++) {
-					 
-						currentNodes.get(j).changeBias(gradient[j][0]);
-						 
-						
-					 }
-				 
-				 
-				 
-				 
-				 
-				 
-				 costPreviousLayer=hiddenErrors;
-				 
-				 
-			 }
-//			 System.out.println(Arrays.toString(cost));
-			 
-		 }
+//		 just comment this line out when using the test set!
+		 writeToFile(storage, n);
 		 
-		 //just comment this line out when using the test set!
-//		 writeToFile(storage, n);
 		 
-		 System.out.println("accuracy of "+right/trainingSize*100+"% ("+right+"out of "+trainingSize+")");
-		 System.out.println("done");
 	 }
 	 static double[][] Multiply(double[][] a,double[][] b) {
 		  double[][] data=new double[a.length][a[0].length];
@@ -372,13 +167,5 @@ public class main {
 		 }
 		 return answer;
 	 }
-	 private static void printMnistMatrix(final MnistMatrix matrix) {
-	        System.out.println("label: " + matrix.getLabel());
-	        for (int r = 0; r < matrix.getNumberOfRows(); r++ ) {
-	            for (int c = 0; c < matrix.getNumberOfColumns(); c++) {
-	                System.out.print(matrix.getValue(r, c) + " ");
-	            }
-	            System.out.println();
-	        }
-	    }
+	
 }
